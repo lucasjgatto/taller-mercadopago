a27ee18a6d7d2d648c26b142239c21167b125ed2
Lucas <lucas.gatto@telefonica.com>
2020-12-04 23:15:11 -0300

    more fixes

--- src/controllers/indexController.js
+++ src/controllers/indexController.js
@@ -3,6 +3,7 @@ const mercadopago = require('mercadopago');
 mercadopago.configure({
     access_token: 'APP_USR-6317427424180639-042414-47e969706991d3a442922b0702a0da44-469485398',
     integrator_id: 'dev_24c65fb163bf11ea96500242ac130004',
+
 });
 
 
@@ -58,27 +59,27 @@ module.exports = {
             auto_return: 'approved',
 
             payer: {
-                name: 'Ryan',
-                surname: 'Dahl',
+                name: 'Lalo',
+                surname: 'Landa',
                 email: 'test_user_63274575@testuser.com',
                 phone: {
                     area_code: '11',
-                    number: 55556666,
+                    number: 22223333,
                 },
                 address: {
-                    zip_code: '1234',
-                    street_name: 'Monroe',
-                    street_number: 860
+                    zip_code: '1111',
+                    street_name: 'False',
+                    street_number: 123
                 }
             },
             payment_methods: {
                 excluded_payment_methods: [
-                    { id: 'visa' }
+                    { id: 'amex' }
                 ],
                 excluded_payment_types: [
                     { id: 'atm' }
                 ],
-                installments: 12
+                installments: 6
             },
             items: [{
                 id: 1234,
